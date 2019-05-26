8ff292f2bd881627276ffef7c6911bbeb0cfa8d7
yogan14 <48348256+yogan14@users.noreply.github.com>
2019-05-26 22:05:59 +0200

    Add files via upload

---
+++ src/photophoto/tempFrame.java
@@ -0,0 +1,14 @@
+package photophoto;
+
+import javax.swing.*;
+
+class tempFrame extends JDialog{
+	private JButton bouton1 = new JButton("image 1");
+	private JButton bouton2 = new JButton("image 2");
+	private JButton bouton3 = new JButton("image 3");
+	
+	
+	public tempFrame() {
+		
+	}
+}
