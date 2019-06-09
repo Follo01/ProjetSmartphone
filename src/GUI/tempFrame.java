260667d3a8a30a762f7c2782a3c2256c510b78bc
yogan14 <48348256+yogan14@users.noreply.github.com>
2019-06-09 18:06:59 +0200

    Add files via upload

--- src/GUI/tempFrame.java
+++ src/GUI/tempFrame.java
@@ -2,8 +2,12 @@ package GUI;
 
 import javax.swing.*;
 
+/**
+ * Classe pour la fenetre de suppression de Photo
+ * @author Diogo
+ * @since 01.06.2019
+ */
 class tempFrame extends JDialog{
-	public tempFrame() {
-		
-	}
+
+	public tempFrame() {}
 }
