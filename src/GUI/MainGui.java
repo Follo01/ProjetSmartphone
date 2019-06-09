738d8764ded8bf4347f2d6fc402b7850d580002a
Follo01 <48347371+Follo01@users.noreply.github.com>
2019-06-09 15:09:09 +0200

    Add files via upload

---
+++ src/GUI/MainGui.java
@@ -0,0 +1,15 @@
+package GUI;
+
+import javax.swing.*;
+import java.io.IOException;
+
+public class MainGui extends JFrame {
+
+    public static void main(String[] args) throws IOException {
+       LockScreen lockScreen = new LockScreen();
+       lockScreen.setVisible(true);
+
+
+
+    }
+}
