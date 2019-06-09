5fb32933b8d8a4175db6bab0e88dacfb604e469f
yogan14 <48348256+yogan14@users.noreply.github.com>
2019-06-09 17:58:12 +0200

    Add files via upload

--- src/GUI/MainGui.java
+++ src/GUI/MainGui.java
@@ -3,13 +3,17 @@ package GUI;
 import javax.swing.*;
 import java.io.IOException;
 
+/**
+ * Classe pour demarrer le telephone
+ * @author Gregory (mise en forme Diogo)
+ * @since 15.04.2019
+ */
 public class MainGui extends JFrame {
-
     public static void main(String[] args) throws IOException {
-       LockScreen lockScreen = new LockScreen();
-       lockScreen.setVisible(true);
-
-
-
+    	/**
+    	 * Affichage du telephone
+    	 */
+    	LockScreen lockScreen = new LockScreen();
+		lockScreen.setVisible(true);
     }
 }
