9873ef604ec70a0fb57375086c487016b7ff2157
yogan14 <48348256+yogan14@users.noreply.github.com>
2019-06-09 18:16:35 +0200

    Add files via upload

--- src/Contact/ListeContact.java
+++ src/Contact/ListeContact.java
@@ -1,58 +1,114 @@
 package Contact;
 
-import GUI.Contact;
-import Serialization.Serialization;
-
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
+import javax.swing.table.TableModel;
+
+import GUI.Contact;
+import Serialization.Serialization;
+import Contact.InfoContact;
+
+/**
+ * Classe pour la sauvegarde des Contacts
+ * @author Gregory (mise en forme Diogo)
+ * @since 01.06.2019
+ */
 
 public class ListeContact {
     Contact Cont;
 
-    //Création de l'ArrayList
-    private List<InfoContact> contacts ;
-    public ListeContact( Contact Cont ) throws IOException{
+    /**
+     * Creation de l ArrayList
+     */
+    private List<InfoContact> contacts;
+    
+    /**
+     * Constructeur de ListeContact
+     * @param Cont le contact
+     * @throws IOException
+     */
+    public ListeContact(Contact Cont) throws IOException {
 
-        //Création du fichier .txt
+        /**
+         * Creation d un fichier .txt
+         */
         creationFichier();
 
         this.Cont = Cont;
 
-        //Déséralisation
+        /**
+         * Deserialisation
+         */
         contacts = (List<InfoContact>) Serialization.deseralisation("src/Contact/contact.txt");
-
     }
 
-    //Ajouter un Contact
-    public void  addContact (int chiffre){
+    /**
+     * Permet d ajouter un contact
+     * @param chiffre du contact
+     */
+    public void addContact(int chiffre) {
         contacts.add(new InfoContact(Cont.getTextNom(), Cont.getTextPrenom(), Cont.getTextTel(), chiffre));
-        //Serialisation
+        
+        /**
+         * Serialisation
+         */
         Serialization.serialisation(contacts, "src/Contact/contact.txt");
     }
 
-    //Suprimer un Contact
-    public void deleteContact (int chiffre){
+    /**
+     * Permet de supprimer un contact
+     * @param chiffre du contact
+     */
+    public void deleteContact(int chiffre) {
         contacts.remove(chiffre);
-        //Serialisation
+        
+        /**
+         * Serialisation
+         */
         Serialization.serialisation(contacts, "src/Contact/contact.txt");
     }
 
-    //Editer un Contact
-    public void editContact (int chiffre, InfoContact ic){
+    /**
+     * Permet d editer un contact
+     * @param chiffre du contact
+     * @param ic
+     * 
+     * Le chiffre et l'info du contact
+     */
+    public void editContact(int chiffre, InfoContact ic) {
         contacts.set(chiffre, ic);
-        //Serialisation
+
+        /**
+         * Serialisation
+         */
         Serialization.serialisation(contacts, "src/Contact/contact.txt");
     }
 
-    //Inserer l'Arraylist dans un tableau pour pouvoir y accéder avec la JTable
-    public Object[][] getArray2D(){
-        Object[][] data = new Object[ contacts.size() ][ 4 ];
+    /**
+     * Permet d inserer l ArrayList dans un tableau
+     * pour pouvoir y acceder avec la JTable
+     * @return les donnees y relatives
+     */
+    public Object[][] getArray2D() {
+        Object[][] data = new Object[contacts.size()][4];
 
         int i = 0;
-        while (i < contacts.size()){
+        while (i < contacts.size()) {
             data[i][0] = contacts.get(i).getNom();
             data[i][1] = contacts.get(i).getPrenom();
             data[i][2] = contacts.get(i).getNumTelephone();
@@ -62,12 +118,14 @@ public class ListeContact {
         return data;
     }
 
-    //Création d'un fichier .txt pour faire la sérialisation (si fichier inexistant)
-    private void creationFichier()
-    {
-        File fichier = new File("src/Contact/contact.txt") ;
-        if (fichier.exists()==false)
-        {
+    /**
+     * Permet de creer un fichier .txt
+     * pour serialiser si le fichier
+     * est inexistant
+     */
+    private void creationFichier() {
+        File fichier = new File("src/Contact/contact.txt");
+        if(fichier.exists()==false) {
             Serialization.serialisation(new ArrayList<InfoContact>(), "src/Contact/contact.txt");
         }
     }
